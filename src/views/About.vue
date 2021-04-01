afebaa02d86b2ac26e49094c94cf69633fcb12d3
Mr.cat <fantasy.lian@163.com>
2021-04-01 17:04:10 +0800

    修改App文件

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>第 {{ year }} 年</p>
+    <p>姓名：{{ name }}</p>
+    <p>年龄：{{ age }}</p>
   </div>
 </template>
+<script>
+import { defineComponent, reactive, ref, toRefs, watch } from 'vue'
+
+export default defineComponent({
+  setup () {
+    const year = ref(0)
+    const state = reactive({ name: 'xiaofeng', age: 20 })
+
+    setInterval(() => {
+      year.value++
+      state.age++
+    }, 1000)
+
+    // watch(
+    //   () => state.age,
+    //   (curAge, preAge) => {
+    //     console.log('新值：', curAge, '旧值：', preAge)
+    //   }
+    // )
+    // watch(year, (newVal, oldVal) => {
+    //   console.log('新值：', newVal, '旧值：', oldVal)
+    // })
+
+    watch([() => state.age, year], ([curAge, newVal], [preAge, oldVal]) => {
+      console.log('新值：', curAge, '旧值：', preAge)
+      console.log('新值：', newVal, '旧值：', oldVal)
+    })
+
+    return {
+      year,
+      ...toRefs(state)
+    }
+  }
+})
+</script>
